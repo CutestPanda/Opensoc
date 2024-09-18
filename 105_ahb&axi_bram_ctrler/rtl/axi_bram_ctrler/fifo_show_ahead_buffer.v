3f70a5997616b638a65f29bddb996d5ed688ea9a
chen_jiayao <2257691535@qq.com>
2024-09-18 15:17:41 +0800

    修改fifo_show_ahead_buffer.v

--- 105_ahb&axi_bram_ctrler/rtl/axi_bram_ctrler/fifo_show_ahead_buffer.v
+++ 105_ahb&axi_bram_ctrler/rtl/axi_bram_ctrler/fifo_show_ahead_buffer.v
@@ -40,7 +40,7 @@ module fifo_show_ahead_buffer #(
     reg std_fifo_rvld; // 标准fifo有效读(指示)
     reg[fifo_data_width-1:0] regs_buffer[1:0]; // 寄存器buffer
     reg[2:0] buffer_data_cnt; // buffer存储计数(3'b001 -> 0, 3'b010 -> 1, 3'b100 -> 2)
-    wire[2:0] buffer_data_cnt_sub1; // buffer存储计数 - 1
+    wire[2:0] buffer_data_cnt_sub1; // buffer存储计数 - 1(3'b001 -> 0, 3'b010 -> 1, 3'b100 -> 2)
     
     assign buffer_data_cnt_sub1 = {buffer_data_cnt[0], buffer_data_cnt[2:1]};
     
@@ -78,11 +78,21 @@ module fifo_show_ahead_buffer #(
     end
     
     /** 标准fifo的读端口 **/
-    assign std_fifo_ren = (buffer_data_cnt[1] & fwft_fifo_ren) | buffer_data_cnt[0];
+    assign std_fifo_ren = buffer_data_cnt[0] | fwft_fifo_ren; // buffer存储计数==0或者FWFT-fifo读使能时标准fifo读使能有效
     
     /** FWFT fifo的读端口 **/
+    reg fwft_fifo_empty_n_reg;
+    
     assign fwft_fifo_dout = regs_buffer[0];
     assign fwft_fifo_empty = buffer_data_cnt[0];
-    assign fwft_fifo_empty_n = ~buffer_data_cnt[0];
+    assign fwft_fifo_empty_n = fwft_fifo_empty_n_reg;
+    
+    always @(posedge clk or negedge rst_n)
+    begin
+        if(~rst_n)
+            fwft_fifo_empty_n_reg <= 1'b0;
+        else if(std_fifo_rvld ^ (fwft_fifo_ren & fwft_fifo_empty_n))
+            # simulation_delay fwft_fifo_empty_n_reg <= std_fifo_rvld ? 1'b1:(~buffer_data_cnt[1]);
+    end
     
 endmodule
