334a4bd664b60f61c786e281b6b5abe5cd853a00
unknown <2257691535@qq.com>
2024-12-03 17:33:10 +0800

    修改驱动程序

--- 402_axi_generic_conv/firmware/zynq_example/main.c
+++ 402_axi_generic_conv/firmware/zynq_example/main.c
@@ -27,6 +27,11 @@
 #define AB_QUAZ_ACC 12 // a/b系数量化精度
 #define C_QUAZ_ACC 14 // c系数量化精度
 
+#define RD_REQ_BUF_ALIGNMENT 64 // 读请求描述子缓存区首地址对齐到的字节数
+#define WT_REQ_BUF_ALIGNMENT 64 // 写请求描述子缓存区首地址对齐到的字节数
+#define MAX_RD_REQ_N 1024 * 1024 * 16 // 最大的读请求个数
+#define MAX_WT_REQ_N 65536 // 最大的写请求个数
+
 #define USE_ACP_PORT // 使用ACP接口
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
