72607c8e7b8a79eee24616e1b9f785dcb44d39f5
unknown <2257691535@qq.com>
2024-12-08 17:29:42 +0800

    上传未完成的EXU

---
+++ 600_panda_risc_v/rtl/exu/panda_risc_v_alu.v
@@ -0,0 +1,184 @@
+`timescale 1ns / 1ps
+/********************************************************************
+本模块: ALU
+
+描述:
+ALU实现了以下运算:
+	按位逻辑运算(XOR, OR, AND)
+	加减法运算(+, -)
+	比较(>=, <, ==, !=)
+	移位(SLL, SRL, SRA)
+
+注意：
+无
+
+协议:
+无
+
+作者: 陈家耀
+日期: 2024/12/07
+********************************************************************/
+
+
+module panda_risc_v_alu(
+	// ALU操作信息输入
+	input wire[3:0] op_mode, // 操作类型
+	input wire[31:0] op1, // 操作数1
+	input wire[31:0] op2, // 操作数2
+	
+	// ALU计算结果输出
+	output wire[31:0] res // 计算结果
+);
+	
+	/** 常量 **/
+	// 操作类型
+	localparam OP_MODE_ADD = 4'd0; // 加法
+	localparam OP_MODE_SUB = 4'd1; // 减法
+	localparam OP_MODE_EQU = 4'd2; // 比较是否相等
+	localparam OP_MODE_NEQU = 4'd3; // 比较是否不等
+	localparam OP_MODE_SGN_LT = 4'd4; // 有符号数比较是否小于
+	localparam OP_MODE_SGN_GET = 4'd5; // 有符号数比较是否大于等于
+	localparam OP_MODE_USGN_LT = 4'd6; // 无符号数比较是否小于
+	localparam OP_MODE_USGN_GET = 4'd7; // 无符号数比较是否大于等于
+	localparam OP_MODE_XOR = 4'd8; // 按位异或
+	localparam OP_MODE_OR = 4'd9; // 按位或
+	localparam OP_MODE_AND = 4'd10; // 按位与
+	localparam OP_MODE_LG_LSH = 4'd11; // 逻辑左移
+	localparam OP_MODE_LG_RSH = 4'd12; // 逻辑右移
+	localparam OP_MODE_ATH_RSH = 4'd13; // 算术右移
+	
+	/** ALU详细操作标志 **/
+	wire is_sub; // 减法操作(标志)
+	wire is_cmp_geq; // 比较是否大于等于操作(标志)
+	wire is_cmp_eq; // 比较是否相等操作(标志)
+	wire is_cmp_neq; // 比较是否不等操作(标志)
+	wire is_cmp_sign; // 有符号比较操作(标志)
+	wire is_sll; // 逻辑左移操作(标志)
+	wire is_sra; // 算术右移操作(标志)
+	
+	assign is_sub = op_mode == OP_MODE_SUB;
+	assign is_cmp_geq = (op_mode == OP_MODE_SGN_GET) | (op_mode == OP_MODE_USGN_GET);
+	assign is_cmp_eq = op_mode == OP_MODE_EQU;
+	assign is_cmp_neq = op_mode == OP_MODE_NEQU;
+	assign is_cmp_sign = (op_mode == OP_MODE_SGN_LT) | (op_mode == OP_MODE_SGN_GET);
+	assign is_sll = op_mode == OP_MODE_LG_LSH;
+	assign is_sra = op_mode == OP_MODE_ATH_RSH;
+	
+	/** 按位异或 **/
+	wire[31:0] xor_res;
+	
+	assign xor_res = op1 ^ op2;
+	
+	/**  加法/减法 **/
+	wire[31:0] add_sub_res;
+	wire[31:0] adder_out;
+	wire adder_to_sub;
+	wire[31:0] adder_op1;
+	wire[31:0] adder_op2;
+	wire adder_cin;
+	
+	assign add_sub_res = adder_out;
+	
+	assign adder_out = adder_op1 + adder_op2 + adder_cin;
+	
+	assign adder_to_sub = is_sub | is_cmp_eq | is_cmp_neq;
+	
+	assign adder_op1 = op1;
+	assign adder_op2 = op2 ^ {32{adder_to_sub}}; // adder_to_sub ? (~op2):op2
+	assign adder_cin = adder_to_sub;
+	
+	/** 有符号/无符号比较是否大于等于/小于 **/
+	wire geq_lt_cmp_res;
+	wire geq_lt_comparer_out;
+	wire[32:0] op1_sign_ext;
+	wire[32:0] op2_sign_ext;
+	
+	assign geq_lt_cmp_res = geq_lt_comparer_out ^ is_cmp_geq; // is_cmp_geq ? (~geq_lt_comparer_out):geq_lt_comparer_out
+	
+	assign geq_lt_comparer_out = $signed(op1_sign_ext) < $signed(op2_sign_ext);
+	
+	assign op1_sign_ext = {is_cmp_sign & op1[31], op1};
+	assign op2_sign_ext = {is_cmp_sign & op2[31], op2};
+	
+	/** 比较是否相等/不等 **/
+	wire eq_neq_cmp_res;
+	wire eq_neq_comparer_out;
+	
+	assign eq_neq_cmp_res = eq_neq_comparer_out ^ is_cmp_neq; // is_cmp_neq ? (~eq_neq_comparer_out):eq_neq_comparer_out
+	
+	assign eq_neq_comparer_out = adder_out == 32'd0;
+	
+	/** 按位或 **/
+	wire[31:0] or_res;
+	
+	assign or_res = op1 | op2;
+	
+	/** 按位与 **/
+	wire[31:0] and_res;
+	
+	assign and_res = op1 & op2;
+	
+	/** 逻辑左移/逻辑右移/算术右移 **/
+	wire[31:0] shift_res;
+	wire[31:0] sll_srl_res;
+	wire[31:0] left_shifter_out;
+	wire[31:0] left_shifter_op1;
+	wire[4:0] left_shifter_op2;
+	wire[31:0] shift_sign_mask;
+	wire[31:0] sra_mask;
+	
+	assign shift_res = sll_srl_res | sra_mask;
+	
+	assign sll_srl_res = is_sll ? left_shifter_out:{
+		left_shifter_out[0], left_shifter_out[1], left_shifter_out[2], left_shifter_out[3], 
+		left_shifter_out[4], left_shifter_out[5], left_shifter_out[6], left_shifter_out[7], 
+		left_shifter_out[8], left_shifter_out[9], left_shifter_out[10], left_shifter_out[11], 
+		left_shifter_out[12], left_shifter_out[13], left_shifter_out[14], left_shifter_out[15], 
+		left_shifter_out[16], left_shifter_out[17], left_shifter_out[18], left_shifter_out[19], 
+		left_shifter_out[20], left_shifter_out[21], left_shifter_out[22], left_shifter_out[23], 
+		left_shifter_out[24], left_shifter_out[25], left_shifter_out[26], left_shifter_out[27], 
+		left_shifter_out[28], left_shifter_out[29], left_shifter_out[30], left_shifter_out[31]
+	};
+	
+	assign left_shifter_out = left_shifter_op1 << left_shifter_op2;
+	
+	assign left_shifter_op1 = is_sll ? op1:{
+		op1[0], op1[1], op1[2], op1[3], op1[4], op1[5], op1[6], op1[7], 
+		op1[8], op1[9], op1[10], op1[11], op1[12], op1[13], op1[14], op1[15], 
+		op1[16], op1[17], op1[18], op1[19], op1[20], op1[21], op1[22], op1[23], 
+		op1[24], op1[25], op1[26], op1[27], op1[28], op1[29], op1[30], op1[31]
+	};
+	assign left_shifter_op2 = op2[4:0];
+	
+	assign shift_sign_mask = ~(32'hffff_ffff >> left_shifter_op2);
+	assign sra_mask = {32{is_sra & op1[31]}} & shift_sign_mask;
+	
+	/** ALU结果MUX **/
+	wire is_logic_op;
+	wire is_add_sub_op;
+	wire is_geq_lt_cmp_op;
+	wire is_eq_neq_cmp_op;
+	wire is_shift_op;
+	wire[31:0] logic_res;
+	
+	assign res = 
+		({32{is_logic_op}} & logic_res) | 
+		({32{is_add_sub_op}} & add_sub_res) | 
+		({32{is_geq_lt_cmp_op}} & {31'd0, geq_lt_cmp_res}) | 
+		({32{is_eq_neq_cmp_op}} & {31'd0, eq_neq_cmp_res}) | 
+		({32{is_shift_op}} & shift_res);
+	
+	assign is_logic_op = (op_mode == OP_MODE_XOR) | (op_mode == OP_MODE_OR) | (op_mode == OP_MODE_AND);
+	assign is_add_sub_op = (op_mode == OP_MODE_ADD) | (op_mode == OP_MODE_SUB);
+	assign is_geq_lt_cmp_op = 
+		(op_mode == OP_MODE_SGN_LT) | (op_mode == OP_MODE_SGN_GET) | 
+		(op_mode == OP_MODE_USGN_LT) | (op_mode == OP_MODE_USGN_GET);
+	assign is_eq_neq_cmp_op =(op_mode == OP_MODE_EQU) | (op_mode == OP_MODE_NEQU);
+	assign is_shift_op = (op_mode == OP_MODE_LG_LSH) | (op_mode == OP_MODE_LG_RSH) | (op_mode == OP_MODE_ATH_RSH);
+	
+	assign logic_res = 
+		({32{op_mode == OP_MODE_XOR}} & xor_res) | 
+		({32{op_mode == OP_MODE_OR}} & or_res) | 
+		({32{op_mode == OP_MODE_AND}} & and_res);
+	
+endmodule
---
+++ 600_panda_risc_v/rtl/exu/panda_risc_v_lsu.v
@@ -0,0 +1,456 @@
+`timescale 1ns / 1ps
+/********************************************************************
+本模块: 加载/存储控制单元
+
+描述:
+接收访存请求, 驱动数据ICB主机, 返回访存结果
+包含深度为4的访存信息表
+
+注意：
+访存地址非对齐时不发起ICB传输
+若发生数据总线响应超时, 则不再接受新的访存请求, 数据ICB主机停止传输
+
+协议:
+ICB MASTER
+
+作者: 陈家耀
+日期: 2024/12/07
+********************************************************************/
+
+
+module panda_risc_v_lsu #(
+	parameter integer dbus_access_timeout_th = 16, // 数据总线访问超时周期数(必须>=1)
+    parameter real simulation_delay = 1 // 仿真延时
+)(
+	// 时钟和复位
+	input wire clk,
+	input wire resetn,
+	
+	// 访存请求
+	input wire s_req_ls_sel, // 加载/存储选择(1'b0 -> 加载, 1'b1 -> 存储)
+	input wire[2:0] s_req_ls_type, // 访存类型
+	input wire[4:0] s_req_rd_id_for_ld, // 用于加载的目标寄存器的索引
+	input wire[31:0] s_req_ls_addr, // 访存地址
+	input wire[31:0] s_req_ls_din, // 写数据
+	input wire s_req_valid,
+	output wire s_req_ready,
+	
+	// 访存结果
+	output wire m_resp_ls_sel, // 加载/存储选择(1'b0 -> 加载, 1'b1 -> 存储)
+	output wire[4:0] m_resp_rd_id_for_ld, // 用于加载的目标寄存器的索引
+	output wire[31:0] m_resp_dout, // 读数据
+	output wire[31:0] m_resp_ls_addr, // 访存地址
+	output wire[1:0] m_resp_err, // 错误类型
+	output wire m_resp_valid,
+	input wire m_resp_ready,
+    
+    // 数据ICB主机
+	// 命令通道
+	output wire[31:0] m_icb_cmd_addr,
+	output wire m_icb_cmd_read,
+	output wire[31:0] m_icb_cmd_wdata,
+	output wire[3:0] m_icb_cmd_wmask,
+	output wire m_icb_cmd_valid,
+	input wire m_icb_cmd_ready,
+	// 响应通道
+	input wire[31:0] m_icb_rsp_rdata,
+	input wire m_icb_rsp_err,
+	input wire m_icb_rsp_valid,
+	output wire m_icb_rsp_ready,
+	
+	// 数据总线访问超时标志
+	output wire dbus_timeout
+);
+	
+    // 计算bit_depth的最高有效位编号(即位数-1)
+    function integer clogb2(input integer bit_depth);
+    begin
+        for(clogb2 = -1;bit_depth > 0;clogb2 = clogb2 + 1)
+			bit_depth = bit_depth >> 1;
+    end
+    endfunction
+	
+	/** 常量 **/
+	// 访存类型
+	localparam LS_TYPE_BYTE = 3'b000;
+	localparam LS_TYPE_HALF_WORD = 3'b001;
+	localparam LS_TYPE_WORD = 3'b010;
+	localparam LS_TYPE_BYTE_UNSIGNED = 3'b100;
+	localparam LS_TYPE_HALF_WORD_UNSIGNED = 3'b101;
+	// 访存应答错误类型
+	localparam DBUS_ACCESS_NORMAL = 2'b00; // 正常
+	localparam DBUS_ACCESS_PC_UNALIGNED = 2'b01; // 访存地址非对齐
+	localparam DBUS_ACCESS_BUS_ERR = 2'b10; // 数据总线访问错误
+	localparam DBUS_ACCESS_TIMEOUT = 2'b11; // 响应超时
+	// 访存信息各项的起始索引
+	localparam LS_MSG_LS_TYPE = 76;
+	localparam LS_MSG_WMASK_SID = 72;
+	localparam LS_MSG_IS_STORE_SID = 71;
+	localparam LS_MSG_RD_ID_SID = 66;
+	localparam LS_MSG_DIN_DOUT_SID = 34;
+	localparam LS_MSG_ADDR_SID = 2;
+	localparam LS_MSG_ERR_SID = 0;
+	// 访存任务生命周期标志位索引
+	localparam LS_TASK_START_STAGE_FID = 0;
+	localparam LS_TASK_CMD_STAGE_FID = 1;
+	localparam LS_TASK_RESP_STAGE_FID = 2;
+	localparam LS_TASK_DONE_STAGE_FID = 3;
+	
+	/** 访存请求前处理 **/
+	wire ls_addr_aligned; // 访存地址对齐(标志)
+	wire[31:0] store_din; // 用于写存储映射的数据
+	wire[3:0] store_wmask; // 用于写存储映射的字节有效掩码
+	
+	assign ls_addr_aligned = 
+		(s_req_ls_type == LS_TYPE_BYTE) | 
+		(s_req_ls_type == LS_TYPE_BYTE_UNSIGNED) | 
+		(((s_req_ls_type == LS_TYPE_HALF_WORD) | (s_req_ls_type == LS_TYPE_HALF_WORD_UNSIGNED)) & (~s_req_ls_addr[0])) | 
+		((s_req_ls_type == LS_TYPE_WORD) & (~s_req_ls_addr[0]) & (~s_req_ls_addr[1]));
+	assign store_din = 
+		({32{s_req_ls_type == LS_TYPE_BYTE}} & {4{s_req_ls_din[7:0]}}) | 
+		({32{s_req_ls_type == LS_TYPE_HALF_WORD}} & {2{s_req_ls_din[15:0]}}) | 
+		({32{s_req_ls_type == LS_TYPE_WORD}} & s_req_ls_din);
+	assign store_wmask = 
+		({32{s_req_ls_type == LS_TYPE_BYTE}} & (4'b0001 << s_req_ls_addr[1:0])) | 
+		({32{s_req_ls_type == LS_TYPE_HALF_WORD}} & (4'b0011 << {s_req_ls_addr[1], 1'b0})) | 
+		({32{s_req_ls_type == LS_TYPE_WORD}} & 4'b1111);
+	
+	/** 访存响应后处理 **/
+	wire[2:0] resp_ls_type; // 当前访存响应对应的访存类型
+	wire[31:0] resp_rdata_algn; // 对齐后的访存读数据
+	wire[31:0] load_dout; // 从存储映射加载的数据
+	
+	assign load_dout = 
+		({32{resp_ls_type == LS_TYPE_BYTE}} & {{24{resp_rdata_algn[7]}}, resp_rdata_algn[7:0]}) | 
+		({32{resp_ls_type == LS_TYPE_HALF_WORD}} & {{16{resp_rdata_algn[15]}}, resp_rdata_algn[15:0]}) | 
+		({32{resp_ls_type == LS_TYPE_WORD}} & resp_rdata_algn) | 
+		({32{resp_ls_type == LS_TYPE_BYTE_UNSIGNED}} & {24'd0, resp_rdata_algn[7:0]}) | 
+		({32{resp_ls_type == LS_TYPE_HALF_WORD_UNSIGNED}} & {16'd0, resp_rdata_algn[15:0]});
+	
+	/** 数据总线访问超时计数器 **/
+	reg[clogb2(dbus_access_timeout_th-1):0] m_icb_timeout_cnt; // 数据ICB主机访问超时计数器
+	reg m_icb_timeout_flag; // 数据ICB主机访问超时标志
+	reg m_icb_timeout_idct; // 数据ICB主机访问超时指示
+	wire m_icb_timeout_cnt_en; // 数据ICB主机访问超时计数器使能标志
+	wire m_icb_timeout_cnt_clr; // 数据ICB主机访问超时计数器清零指示
+	
+	assign dbus_timeout = m_icb_timeout_flag;
+	
+	// 数据ICB主机访问超时计数器
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			m_icb_timeout_cnt <= 0;
+		else if((~m_icb_timeout_flag) & (m_icb_timeout_cnt_clr | m_icb_timeout_cnt_en))
+			// m_icb_timeout_cnt_clr ? 0:(m_icb_timeout_cnt + 1)
+			m_icb_timeout_cnt <= # simulation_delay 
+				{(clogb2(dbus_access_timeout_th-1)+1){~m_icb_timeout_cnt_clr}} & (m_icb_timeout_cnt + 1);
+	end
+	
+	// 数据ICB主机访问超时标志
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			m_icb_timeout_flag <= 1'b0;
+		else if(~m_icb_timeout_flag)
+			m_icb_timeout_flag <= # simulation_delay 
+				(~m_icb_timeout_cnt_clr) & m_icb_timeout_cnt_en & (m_icb_timeout_cnt == (dbus_access_timeout_th - 1));
+	end
+	
+	// 数据ICB主机访问超时指示
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			m_icb_timeout_idct <= 1'b0;
+		else
+			m_icb_timeout_idct <= # simulation_delay 
+				(~m_icb_timeout_flag) & 
+				(~m_icb_timeout_cnt_clr) & m_icb_timeout_cnt_en & (m_icb_timeout_cnt == (dbus_access_timeout_th - 1));
+	end
+	
+	/** 访存信息表 **/
+	// 存储寄存器组
+	reg[78:0] ls_msg_table[0:3]; // 访存信息寄存器组
+	reg[3:0] ls_task_life_cycle_vec[0:3]; // 访存任务生命周期向量寄存器组
+	// 滞外统计
+	reg[2:0] launched_ls_task_n; // 已启动的访存任务个数
+	reg[2:0] dbus_transmitting_ls_task_n; // 正在数据总线上进行传输的访存任务个数
+	// 启动阶段
+	wire launch_new_ls_task; // 启动新的访存任务(指示)
+	reg ls_msg_table_empty_n; // 访存信息表空标志
+	reg ls_msg_table_full_n; // 访存信息表满标志
+	reg[3:0] ls_msg_table_wptr; // 访存信息表独热码写指针
+	// 命令阶段
+	wire start_icb_trans; // 启动新的ICB传输(指示)
+	reg no_dbus_transmitting_ls_task; // 没有正在数据总线上进行传输的访存任务(标志)
+	wire ls_task_skip_cmd_stage; // 当前访存任务跳过命令阶段(指示)
+	wire ls_task_fns_cmd_stage; // 当前访存任务完成命令阶段(指示)
+	reg[1:0] ls_msg_table_cmd_stage_rptr; // 访存信息表命令阶段读指针
+	// 响应阶段
+	wire icb_resp_gotten; // 获得ICB响应(指示)
+	wire ls_task_fns_resp_stage; // 当前访存任务完成响应阶段(指示)
+	wire ls_task_skip_resp_stage; // 当前访存任务跳过响应阶段(指示)
+	reg[3:0] ls_msg_table_resp_stage_wptr; // 访存信息表响应阶段独热码写指针
+	// 完成阶段
+	wire retire_ls_task; // 访存任务退休(指示)
+	reg[1:0] ls_msg_table_rptr; // 访存信息表读指针
+	
+	// 发生数据总线响应超时后, 不再接受新的访存请求
+	assign s_req_ready = (~m_icb_timeout_flag) & ls_msg_table_full_n;
+	
+	assign m_resp_ls_sel = ls_msg_table[ls_msg_table_rptr][LS_MSG_IS_STORE_SID];
+	assign m_resp_rd_id_for_ld = ls_msg_table[ls_msg_table_rptr][LS_MSG_RD_ID_SID+4:LS_MSG_RD_ID_SID];
+	assign m_resp_dout = load_dout;
+	assign m_resp_ls_addr = ls_msg_table[ls_msg_table_rptr][LS_MSG_ADDR_SID+31:LS_MSG_ADDR_SID];
+	assign m_resp_err = ls_msg_table[ls_msg_table_rptr][LS_MSG_ERR_SID+1:LS_MSG_ERR_SID];
+	assign m_resp_valid = ls_task_life_cycle_vec[ls_msg_table_rptr][LS_TASK_DONE_STAGE_FID];
+	
+	/*
+	当访存信息表空时, 将访存请求直接旁路给数据ICB主机的命令通道
+	访存地址非对齐时不发起ICB传输
+	发生数据总线响应超时后, 不再发起新的ICB传输
+	*/
+	assign m_icb_cmd_addr = ls_msg_table_empty_n ? 
+		ls_msg_table[ls_msg_table_cmd_stage_rptr][LS_MSG_ADDR_SID+31:LS_MSG_ADDR_SID]:s_req_ls_addr;
+	assign m_icb_cmd_read = ls_msg_table_empty_n ? 
+		(~ls_msg_table[ls_msg_table_cmd_stage_rptr][LS_MSG_IS_STORE_SID]):(~s_req_ls_sel);
+	assign m_icb_cmd_wdata = ls_msg_table_empty_n ? 
+		ls_msg_table[ls_msg_table_cmd_stage_rptr][LS_MSG_DIN_DOUT_SID+31:LS_MSG_DIN_DOUT_SID]:store_din;
+	assign m_icb_cmd_wmask = ls_msg_table_empty_n ? 
+		ls_msg_table[ls_msg_table_cmd_stage_rptr][LS_MSG_WMASK_SID+3:LS_MSG_WMASK_SID]:store_wmask;
+	assign m_icb_cmd_valid = (~m_icb_timeout_flag) & (ls_msg_table_empty_n ? 
+		ls_task_life_cycle_vec[ls_msg_table_cmd_stage_rptr][LS_TASK_CMD_STAGE_FID]:(s_req_valid & ls_addr_aligned));
+	
+	/*
+	ICB响应通道的ready无需等待当前访存任务处于响应阶段
+	发生数据总线响应超时后, 不再接受新的ICB响应
+	*/
+	assign m_icb_rsp_ready = ~m_icb_timeout_flag;
+	
+	assign resp_ls_type = ls_msg_table[ls_msg_table_rptr][LS_MSG_LS_TYPE+2:LS_MSG_LS_TYPE];
+	assign resp_rdata_algn = 
+		ls_msg_table[ls_msg_table_rptr][LS_MSG_DIN_DOUT_SID+31:LS_MSG_DIN_DOUT_SID] >> 
+			{ls_msg_table[ls_msg_table_rptr][LS_MSG_ADDR_SID+1:LS_MSG_ADDR_SID], 3'b000};
+	
+	assign m_icb_timeout_cnt_en = ~no_dbus_transmitting_ls_task;
+	assign m_icb_timeout_cnt_clr = m_icb_rsp_valid & m_icb_rsp_ready;
+	
+	assign launch_new_ls_task = s_req_valid & s_req_ready;
+	assign start_icb_trans = m_icb_cmd_valid & m_icb_cmd_ready;
+	assign ls_task_skip_cmd_stage = 
+		launch_new_ls_task & // 启动新的访存任务
+		((~ls_addr_aligned) | // 访存地址非对齐
+		((~ls_msg_table_empty_n) & m_icb_cmd_ready)); // 旁路的访存请求被数据ICB主机的命令通道接受
+	assign ls_task_fns_cmd_stage = 
+		// 访存信息表项被数据ICB主机的命令通道接受
+		(~m_icb_timeout_flag) & ls_msg_table_empty_n & 
+		ls_task_life_cycle_vec[ls_msg_table_cmd_stage_rptr][LS_TASK_CMD_STAGE_FID] & m_icb_cmd_ready;
+	assign icb_resp_gotten = 
+		(m_icb_rsp_valid & m_icb_rsp_ready) | // ICB主机返回响应
+		m_icb_timeout_idct; // 响应超时
+	assign ls_task_fns_resp_stage = icb_resp_gotten;
+	assign ls_task_skip_resp_stage = 
+		launch_new_ls_task & // 启动新的访存任务
+		(~ls_addr_aligned); // 访存地址非对齐
+	assign retire_ls_task = m_resp_valid & m_resp_ready;
+	
+	// 已启动的访存任务个数
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			launched_ls_task_n <= 3'b000;
+		else if(launch_new_ls_task ^ retire_ls_task)
+			// launch_new_ls_task ? (launched_ls_task_n + 3'b001):(launched_ls_task_n - 3'b001)
+			launched_ls_task_n <= # simulation_delay launched_ls_task_n + {{2{~launch_new_ls_task}}, 1'b1};
+	end
+	// 正在数据总线上进行传输的访存任务个数
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			dbus_transmitting_ls_task_n <= 3'b000;
+		else if(start_icb_trans ^ icb_resp_gotten)
+			// start_icb_trans ? (dbus_transmitting_ls_task_n + 3'b001):(dbus_transmitting_ls_task_n - 3'b001)
+			dbus_transmitting_ls_task_n <= # simulation_delay dbus_transmitting_ls_task_n + {{2{~start_icb_trans}}, 1'b1};
+	end
+	
+	// 访存信息表空标志
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_empty_n <= 1'b0;
+		else if(launch_new_ls_task ^ retire_ls_task)
+			// launch_new_ls_task ? 1'b1:(launched_ls_task_n != 3'b001)
+			ls_msg_table_empty_n <= # simulation_delay launch_new_ls_task | (~launched_ls_task_n[0]) | launched_ls_task_n[1];
+	end
+	// 访存信息表满标志
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_full_n <= 1'b1;
+		else if(launch_new_ls_task ^ retire_ls_task)
+			// launch_new_ls_task ? (launched_ls_task_n != 3'b011):1'b1
+			ls_msg_table_full_n <= # simulation_delay ~(launch_new_ls_task & launched_ls_task_n[0] & launched_ls_task_n[1]);
+	end
+	// 访存信息表独热码写指针
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_wptr <= 4'b0001;
+		else if(launch_new_ls_task)
+			ls_msg_table_wptr <= # simulation_delay {ls_msg_table_wptr[2:0], ls_msg_table_wptr[3]};
+	end
+	
+	// 没有正在数据总线上进行传输的访存任务(标志)
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			no_dbus_transmitting_ls_task <= 1'b1;
+		else if(start_icb_trans ^ icb_resp_gotten)
+			// start_icb_trans ? 1'b0:(dbus_transmitting_ls_task_n == 3'b001)
+			no_dbus_transmitting_ls_task <= # simulation_delay 
+				(~start_icb_trans) & dbus_transmitting_ls_task_n[0] & (~dbus_transmitting_ls_task_n[1]);	
+	end
+	
+	// 访存信息表命令阶段读指针
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_cmd_stage_rptr <= 2'b00;
+		else if(ls_task_skip_cmd_stage | ls_task_fns_cmd_stage)
+			ls_msg_table_cmd_stage_rptr <= # simulation_delay ls_msg_table_cmd_stage_rptr + 2'b01;
+	end
+	
+	// 访存信息表响应阶段独热码写指针
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_resp_stage_wptr <= 4'b0001;
+		else if(ls_task_fns_resp_stage | ls_task_skip_resp_stage)
+			ls_msg_table_resp_stage_wptr <= # simulation_delay {ls_msg_table_resp_stage_wptr[2:0], ls_msg_table_resp_stage_wptr[3]};
+	end
+	
+	// 访存信息表读指针
+	always @(posedge clk or negedge resetn)
+	begin
+		if(~resetn)
+			ls_msg_table_rptr <= 2'b00;
+		else if(retire_ls_task)
+			ls_msg_table_rptr <= # simulation_delay ls_msg_table_rptr + 2'b01;
+	end
+	
+	// 写访存信息寄存器组
+	genvar ls_msg_table_i;
+	generate
+		for(ls_msg_table_i = 0;ls_msg_table_i < 4;ls_msg_table_i = ls_msg_table_i + 1)
+		begin
+			// 错误类型
+			always @(posedge clk)
+			begin
+				if((ls_task_fns_resp_stage | ls_task_skip_resp_stage) & ls_msg_table_resp_stage_wptr[ls_msg_table_i])
+					ls_msg_table[ls_msg_table_i][LS_MSG_ERR_SID+1:LS_MSG_ERR_SID] <= # simulation_delay 
+						({2{ls_task_skip_resp_stage}} & DBUS_ACCESS_PC_UNALIGNED) | 
+						({2{m_icb_rsp_valid & m_icb_rsp_ready & m_icb_rsp_err}} & DBUS_ACCESS_BUS_ERR) | 
+						({2{m_icb_rsp_valid & m_icb_rsp_ready & (~m_icb_rsp_err)}} & DBUS_ACCESS_NORMAL) | 
+						({2{m_icb_timeout_idct}} & DBUS_ACCESS_TIMEOUT);
+			end
+			
+			// 访存地址
+			always @(posedge clk)
+			begin
+				if(launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) // 启动阶段时载入
+					ls_msg_table[ls_msg_table_i][LS_MSG_ADDR_SID+31:LS_MSG_ADDR_SID] <= # simulation_delay 
+						s_req_ls_addr;
+			end
+			
+			// 访存数据
+			always @(posedge clk)
+			begin
+				if((launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) | 
+					((m_icb_rsp_valid & m_icb_rsp_ready) & (~ls_task_skip_resp_stage) & ls_msg_table_resp_stage_wptr[ls_msg_table_i]))
+					ls_msg_table[ls_msg_table_i][LS_MSG_DIN_DOUT_SID+31:LS_MSG_DIN_DOUT_SID] <= # simulation_delay 
+						(m_icb_rsp_valid & m_icb_rsp_ready) ? m_icb_rsp_rdata:store_din;
+			end
+			
+			// 用于加载的目标寄存器的索引
+			always @(posedge clk)
+			begin
+				if(launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) // 启动阶段时载入
+					ls_msg_table[ls_msg_table_i][LS_MSG_RD_ID_SID+4:LS_MSG_RD_ID_SID] <= # simulation_delay 
+						s_req_rd_id_for_ld;
+			end
+			
+			// 加载/存储选择(1'b0 -> 加载, 1'b1 -> 存储)
+			always @(posedge clk)
+			begin
+				if(launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) // 启动阶段时载入
+					ls_msg_table[ls_msg_table_i][LS_MSG_IS_STORE_SID] <= # simulation_delay 
+						s_req_ls_sel;
+			end
+			
+			// 用于写存储映射的字节有效掩码
+			always @(posedge clk)
+			begin
+				if(launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) // 启动阶段时载入
+					ls_msg_table[ls_msg_table_i][LS_MSG_WMASK_SID+3:LS_MSG_WMASK_SID] <= # simulation_delay 
+						store_wmask;
+			end
+			
+			// 访存类型
+			always @(posedge clk)
+			begin
+				if(launch_new_ls_task & ls_msg_table_wptr[ls_msg_table_i]) // 启动阶段时载入
+					ls_msg_table[ls_msg_table_i][LS_MSG_LS_TYPE+2:LS_MSG_LS_TYPE] <= # simulation_delay 
+						s_req_ls_type;
+			end
+		end
+	endgenerate
+	
+	// 访存任务生命周期向量寄存器组
+	genvar ls_task_life_cycle_vec_i;
+	generate
+		for(ls_task_life_cycle_vec_i = 0;ls_task_life_cycle_vec_i < 4;ls_task_life_cycle_vec_i = ls_task_life_cycle_vec_i + 1)
+		begin
+			always @(posedge clk or negedge resetn)
+			begin
+				if(~resetn)
+					ls_task_life_cycle_vec[ls_task_life_cycle_vec_i] <= 4'b0001;
+				else if(
+				// 启动新的访存任务
+				(ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_START_STAGE_FID] & 
+					ls_msg_table_wptr[ls_task_life_cycle_vec_i] & launch_new_ls_task) | 
+				// 数据ICB主机命令通道完成传输
+				(ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_CMD_STAGE_FID] & 
+					(ls_msg_table_cmd_stage_rptr == ls_task_life_cycle_vec_i) & ls_task_fns_cmd_stage) | 
+				// 数据ICB主机响应通道完成传输
+				(ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_RESP_STAGE_FID] & 
+					ls_msg_table_resp_stage_wptr[ls_task_life_cycle_vec_i] & ls_task_fns_resp_stage) | 
+				// 后级取走访存结果
+				(ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_DONE_STAGE_FID] & 
+					(ls_msg_table_rptr == ls_task_life_cycle_vec_i) & retire_ls_task))
+					ls_task_life_cycle_vec[ls_task_life_cycle_vec_i] <= # simulation_delay 
+						// 现处于开始阶段, 不跳过命令阶段
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_START_STAGE_FID] & 
+							(~ls_task_skip_cmd_stage)}} & 4'b0010) | 
+						// 现处于开始阶段, 跳过命令阶段, 不跳过响应阶段, 在请求旁路到数据ICB主机命令通道并被立即接受时发生
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_START_STAGE_FID] & 
+							ls_task_skip_cmd_stage & (~ls_task_skip_resp_stage)}} & 4'b0100) | 
+						// 现处于开始阶段, 跳过命令阶段, 跳过响应阶段, 在访存地址非对齐时发生
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_START_STAGE_FID] & 
+							ls_task_skip_cmd_stage & ls_task_skip_resp_stage}} & 4'b1000) | 
+						// 现处于命令阶段, 不跳过响应阶段
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_CMD_STAGE_FID] & 
+							(~ls_task_fns_resp_stage)}} & 4'b0100) | 
+						// 现处于命令阶段, 跳过响应阶段, 在数据ICB主机立即返回当前传输的响应时发生
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_CMD_STAGE_FID] & 
+							ls_task_fns_resp_stage}} & 4'b1000) | 
+						// 现处于响应阶段, 直接更新为完成阶段
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_RESP_STAGE_FID]}} & 
+							4'b1000) | 
+						// 现处于完成阶段, 直接更新为开始阶段
+						({4{ls_task_life_cycle_vec[ls_task_life_cycle_vec_i][LS_TASK_DONE_STAGE_FID]}} & 
+							4'b0001);
+			end
+		end
+	endgenerate
+    
+endmodule
