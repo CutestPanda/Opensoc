bab80ebef5cdfa5d88ed9095f4a39f3835fe12ad
unknown <2257691535@qq.com>
2024-12-14 12:13:53 +0800

    更新EXU中的多周期除法器

--- 600_panda_risc_v/rtl/exu/panda_risc_v_divider.v
+++ 600_panda_risc_v/rtl/exu/panda_risc_v_divider.v
@@ -118,38 +118,66 @@ module panda_risc_v_divider #(
 	wire[32:0] divisor; // 除数
 	reg[32:0] remainder; // 余数寄存器
 	reg[32:0] quotient; // 商寄存器
+	wire[32:0] remainder_div_exct_fix; // 整除修正后的余数
+	wire[32:0] quotient_div_exct_fix; // 整除修正后的商
 	wire[32:0] remainder_add_op_a; // 余数加法器的操作数A
 	wire[32:0] remainder_add_op_b; // 余数加法器的操作数B
 	wire remainder_add_cin; // 余数加法器的进位输入
 	wire qut_for_shift_in; // 上商值
-	wire to_fix_remainder; // 需要修正余数(标志)
-	wire to_fix_quotient; // 需要修正商(标志)
+	wire need_fix_remainder; // 需要修正余数(标志)
+	wire need_fix_quotient; // 需要修正商(标志)
+	reg to_fix_remainder; // 进行修正余数(标志)
+	reg to_fix_quotient; // 进行修正商(标志)
+	wire to_fix_div_exct; // 进行整除修正(标志)
 	wire div_zero; // 除0标志
 	wire div_ovf; // 除法溢出标志
+	wire rem_eq_dvs; // 余数等于除数(标志)
+	wire rem_eq_inv_dvs; // 余数等于负的除数(标志)
 	reg[36:0] div_proc_onehot; // 除法计算流程独热码
 	
-	assign div_res_s0_data = div_in_buf_dout[DIV_IN_MSG_REM_SEL] ? remainder[31:0]:quotient[31:0];
+	assign div_in_buf_ren = div_proc_onehot[36] & div_res_s0_ready;
+	
+	assign div_res_s0_data = div_in_buf_dout[DIV_IN_MSG_REM_SEL] ? remainder_div_exct_fix[31:0]:quotient_div_exct_fix[31:0];
 	assign div_res_s0_valid = div_proc_onehot[36];
 	
 	assign dividend = div_in_buf_dout[DIV_IN_MSG_OP_A+32:DIV_IN_MSG_OP_A];
 	assign divisor = div_in_buf_dout[DIV_IN_MSG_OP_B+32:DIV_IN_MSG_OP_B];
 	
+	// 进行整除修正, 余数置0
+	assign remainder_div_exct_fix = {33{~to_fix_div_exct}} & remainder;
+	// 进行整除修正, 被除数和除数异号时减去1, 被除数和除数同号时加上1
+	assign quotient_div_exct_fix = 
+		quotient + {{32{to_fix_div_exct & (dividend[32] ^ divisor[32])}}, to_fix_div_exct};
+	
 	assign remainder_add_op_a = 
-		({33{div_proc_onehot[0]}} & {33{dividend[32]}}) | 
-		({33{div_proc_onehot[35]}} & remainder) | 
-		({33{(~div_proc_onehot[0]) & (~div_proc_onehot[35])}} & {remainder[31:0], quotient[32]});
-	assign remainder_add_op_b = {33{remainder_add_cin}} ^ divisor;
+		({33{div_proc_onehot[0]}} & {33{dividend[32]}}) | // 预置阶段 -> 将符号拓展后的被除数载入{R, Q}
+		({33{div_proc_onehot[35]}} & remainder) | // 修正阶段 -> 修正余数
+		({33{(~div_proc_onehot[0]) & (~div_proc_onehot[35])}} & {remainder[31:0], quotient[32]}); // 计算阶段 -> 左移{R, Q}
+	assign remainder_add_op_b = {33{remainder_add_cin}} ^ divisor; // 除数
 	assign remainder_add_cin = qut_for_shift_in;
 	
-	assign qut_for_shift_in = ~(((div_proc_onehot[0] | div_proc_onehot[35]) ? dividend[32]:remainder[32]) ^ divisor[32]);
+	/*
+	预置或修正阶段时判断被除数与除数是否异号, 计算阶段时判断余数与除数是否异号
+	预置或计算阶段时异号相加, 同号相减; 修正阶段时同号相加, 异号相减
+	*/
+	assign qut_for_shift_in = (~div_proc_onehot[35]) ^ 
+		(((div_proc_onehot[0] | div_proc_onehot[35]) ? dividend[32]:remainder[32]) ^ divisor[32]);
 	
-	assign to_fix_remainder = remainder[32] ^ divisor[32];
-	assign to_fix_quotient = dividend[32] ^ divisor[32];
+	// 仅左移商阶段时, 若余数与被除数异号则需要修正余数
+	assign need_fix_remainder = remainder[32] ^ dividend[32];
+	// 仅左移商阶段时, 若被除数与除数异号则需要修正商
+	assign need_fix_quotient = dividend[32] ^ divisor[32];
+	
+	// 当余数等于除数或者负的除数时, 进行整除修正
+	assign to_fix_div_exct = rem_eq_dvs | rem_eq_inv_dvs;
 	
 	assign div_zero = divisor[31:0] == 32'h0000_0000;
 	assign div_ovf = 
 		(dividend[31:0] == 32'h8000_0000) & // 被除数为-2^31
 		(divisor[31:0] == 32'hFFFF_FFFF); // 除数为-1
+	
+	assign rem_eq_dvs = remainder == divisor;
+	assign rem_eq_inv_dvs = remainder == ((~divisor) + 1'b1);
 	
 	// 余数寄存器
 	always @(posedge clk)
@@ -171,9 +199,25 @@ module panda_risc_v_divider #(
 			((~div_proc_onehot[0]) & (~div_proc_onehot[35]) & (~div_proc_onehot[36])) | 
 			(div_proc_onehot[35] & to_fix_quotient))
 			quotient <= # simulation_delay 
+				// 预置阶段
 				(({33{div_proc_onehot[0]}} & ({33{div_zero}} | dividend)) | // 除0时将商的所以位设为1, 溢出时商为-2^31
+				// 修正阶段
 				({33{div_proc_onehot[35]}} & quotient) | 
+				// 计算阶段, 仅左移商阶段
 				({33{(~div_proc_onehot[0]) & (~div_proc_onehot[35])}} & {quotient[31:0], qut_for_shift_in})) + div_proc_onehot[35];
+	end
+	
+	// 进行修正余数(标志)
+	always @(posedge clk)
+	begin
+		if(div_proc_onehot[34])
+			to_fix_remainder <= # simulation_delay need_fix_remainder;
+	end
+	// 进行修正商(标志)
+	always @(posedge clk)
+	begin
+		if(div_proc_onehot[34])
+			to_fix_quotient <= # simulation_delay need_fix_quotient;
 	end
 	
 	// 除法计算流程独热码
@@ -186,11 +230,11 @@ module panda_risc_v_divider #(
 			(div_proc_onehot[36] & div_res_s0_ready))
 			div_proc_onehot <= # simulation_delay 
 				({37{div_proc_onehot[0]}} & ((div_zero | div_ovf) ? 
-					37'b1_0000_0000_0000_0000_0000_0000_0000_0000_0000: // 除0或溢出
+					37'b1_0000_0000_0000_0000_0000_0000_0000_0000_0000: // 除0或溢出, 直接输出
 					37'b0_0000_0000_0000_0000_0000_0000_0000_0000_0010)) | 
-				({37{div_proc_onehot[34]}} & ((to_fix_remainder | to_fix_quotient) ? 
+				({37{div_proc_onehot[34]}} & ((need_fix_remainder | need_fix_quotient) ? 
 					37'b0_1000_0000_0000_0000_0000_0000_0000_0000_0000: // 需要修正余数或商
-					37'b1_0000_0000_0000_0000_0000_0000_0000_0000_0000)) | // 不需要修正余数或商
+					37'b1_0000_0000_0000_0000_0000_0000_0000_0000_0000)) | // 不需要修正余数或商, 直接输出
 				({37{(~div_proc_onehot[0]) & (~div_proc_onehot[34])}} & {div_proc_onehot[35:0], div_proc_onehot[36]});
 	end
 	
