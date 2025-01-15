5ddea662e1c066bfedf8ea77a8670d5e599bbb35
unknown <2257691535@qq.com>
2025-01-15 11:38:08 +0800

    为CPU核顶层的testbench增加risc-v tests测试成功/失败的打印信息

--- 600_panda_risc_v/tb/tb_panda_risc_v/tb_panda_risc_v.sv
+++ 600_panda_risc_v/tb/tb_panda_risc_v/tb_panda_risc_v.sv
@@ -6,8 +6,8 @@ module tb_panda_risc_v();
 	// 待测模块配置
 	localparam integer IMEM_DEPTH = 8 * 1024; // 指令存储器深度
 	localparam integer DMEM_DEPTH = 8 * 1024; // 数据存储器深度
-	localparam IMEM_INIT_FILE = "E:/modelsim/tb_panda_risc_v/inst_test/rv32um-p-div.txt"; // 指令存储器的初始化文件路径
-	localparam DMEM_INIT_FILE = "E:/modelsim/tb_panda_risc_v/inst_test/rv32um-p-div.txt"; // 数据存储器的初始化文件路径
+	localparam IMEM_INIT_FILE = "E:/modelsim/tb_panda_risc_v/inst_test/rv32um-p-remu.txt"; // 指令存储器的初始化文件路径
+	localparam DMEM_INIT_FILE = "E:/modelsim/tb_panda_risc_v/inst_test/rv32um-p-remu.txt"; // 数据存储器的初始化文件路径
 	// 时钟和复位配置
 	localparam real clk_p = 10.0; // 时钟周期
 	localparam real simulation_delay = 1.0; // 仿真延时
@@ -32,6 +32,57 @@ module tb_panda_risc_v();
 		# (clk_p * 10 + simulation_delay);
 		
 		rst_n <= 1'b1;
+	end
+	
+	/** 汇编程序测试 **/
+	wire[31:0] x3;
+	wire[31:0] x26;
+	wire[31:0] x27;
+	
+	assign x3 = panda_risc_v_sim_u.panda_risc_v_u.panda_risc_v_exu_u.panda_risc_v_reg_file_u.generic_reg_file[3];
+	assign x26 = panda_risc_v_sim_u.panda_risc_v_u.panda_risc_v_exu_u.panda_risc_v_reg_file_u.generic_reg_file[26];
+	assign x27 = panda_risc_v_sim_u.panda_risc_v_u.panda_risc_v_exu_u.panda_risc_v_reg_file_u.generic_reg_file[27];
+	
+	initial
+	begin
+		$display("test running...");
+		
+		wait(x26 == 32'b1);
+		
+        # (clk_p * 10);
+		
+        if(x27 == 32'b1)
+		begin
+            $display("~~~~~~~~~~~~~~~~~~~ TEST_PASS ~~~~~~~~~~~~~~~~~~~");
+            $display("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            $display("~~~~~~~~~ #####     ##     ####    #### ~~~~~~~~~");
+            $display("~~~~~~~~~ #    #   #  #   #       #     ~~~~~~~~~");
+            $display("~~~~~~~~~ #    #  #    #   ####    #### ~~~~~~~~~");
+            $display("~~~~~~~~~ #####   ######       #       #~~~~~~~~~");
+            $display("~~~~~~~~~ #       #    #  #    #  #    #~~~~~~~~~");
+            $display("~~~~~~~~~ #       #    #   ####    #### ~~~~~~~~~");
+            $display("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        end
+		else
+		begin
+		    $display("~~~~~~~~~~~~~~~~~~~ TEST_FAIL ~~~~~~~~~~~~~~~~~~~~");
+            $display("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            $display("~~~~~~~~~~######    ##       #    #     ~~~~~~~~~~");
+            $display("~~~~~~~~~~#        #  #      #    #     ~~~~~~~~~~");
+            $display("~~~~~~~~~~#####   #    #     #    #     ~~~~~~~~~~");
+            $display("~~~~~~~~~~#       ######     #    #     ~~~~~~~~~~");
+            $display("~~~~~~~~~~#       #    #     #    #     ~~~~~~~~~~");
+            $display("~~~~~~~~~~#       #    #     #    ######~~~~~~~~~~");
+            $display("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			
+            $display("fail testnum = %2d", x3);
+			
+            for(int i = 0;i < 32;i++)
+			begin
+				$display("x%2d = 0x%x", i, 
+					panda_risc_v_sim_u.panda_risc_v_u.panda_risc_v_exu_u.panda_risc_v_reg_file_u.generic_reg_file[i]);
+			end
+        end
 	end
 	
 	/** 待测模块 **/
